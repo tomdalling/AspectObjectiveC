f8e336c4d1a3b923a46004241ba8b3f3e2ff2602
Tom Dalling <tom.dalling@gmail.com>
2010-03-17 19:17:14 +1000

    Added a build step for the unit test target that automatically runs generate_method_hooking_tests.rb and inserts the output into TestAOCMethodHooking.m

--- AspectObjectiveC/src/AOCMethodHooking.h
+++ AspectObjectiveC/src/AOCMethodHooking.h
@@ -6,5 +6,7 @@ typedef void(*AOCMethodInvocationHook)(NSInvocation*);
 BOOL AOCInstallHook(Class cls, SEL selector, NSError** outError);
 void AOCUninstallHook(Class cls, SEL selector);
 BOOL AOCIsHookInstalled(Class cls, SEL selector);
+//TODO: BOOL AOCInstallValueForKeyOverride(Class cls);
+//TODO: BOOL AOCUninstallValueForKeyOverride(Class cls);
 AOCMethodInvocationHook AOCGlobalInvocationHook();
 void AOCSetGlobalInvocationHook(AOCMethodInvocationHook hook);
---
+++ AspectObjectiveC/tests/insert_method_hooking_tests.rb
@@ -0,0 +1,54 @@
+
+FILE_NAME_FOR_INSERT = ARGV[0]
+BEGIN_INSERT_LINE = "//begin #{File.basename(__FILE__)}"
+END_INSERT_LINE = "//end #{File.basename(__FILE__)}"
+
+if(!File.exists?(FILE_NAME_FOR_INSERT) || !File.writable?(FILE_NAME_FOR_INSERT))
+    abort("Can't find or can't write to #{FILE_NAME_FOR_INSERT}")
+end
+
+newFileContent = ""
+fileToModify = File.new(FILE_NAME_FOR_INSERT, "r")
+
+#Include the old stuff up to BEGIN_INSERT_LINE of file
+while true
+    line = fileToModify.gets
+    if line.nil?
+        abort("hit EOF before finding \"#{BEGIN_INSERT_LINE}\"") 
+    elsif line.chomp == BEGIN_INSERT_LINE
+        break
+    else
+        newFileContent += line
+    end
+end
+
+newFileContent += BEGIN_INSERT_LINE + "\n"
+
+#Include the new stuff from STDIN
+while line = STDIN.gets
+    newFileContent += line
+end
+
+#EXCLUDE the old stuff up to END_INSERT_LINE of file
+while true
+    line = fileToModify.gets
+    if line.nil?
+        abort("hit EOF before finding \"#{END_INSERT_LINE}\"")
+    elsif line.chomp == END_INSERT_LINE
+        break
+    end
+end
+
+newFileContent += END_INSERT_LINE + "\n"
+
+#include the old stuff after END_INSERT_LINE of file
+while line = fileToModify.gets
+    newFileContent += line
+end
+
+#write new file contents over old
+fileToModify.close
+fileToModify = File.new(FILE_NAME_FOR_INSERT, "w")
+fileToModify.write(newFileContent)
+fileToModify.close
+
