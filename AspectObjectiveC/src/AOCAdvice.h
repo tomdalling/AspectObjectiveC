0bef41e504eb4922320ddae3cc9715df2d1b514a
Tom Dalling <tom.dalling@gmail.com>
2010-04-01 02:00:14 +1000

    Added a bunch of new documentation with Doxygen

--- AspectObjectiveC/src/AOCAdvice.h
+++ AspectObjectiveC/src/AOCAdvice.h
@@ -3,76 +3,108 @@
 #import "AOCInvocationProtocol.h"
 
 /*!
-    @abstract    The protocol implemented by all advice objects.
-    @discussion  All methods are optional.
+    The protocol implemented by all advice objects.
+
+    In all of the methods, <tt>[inv imp]</tt> is the actual IMP that the advice is running for.
+    This will be different from <tt>[[inv target] methodForSelector:[inv selector]]</tt>
+    because the method has been swizzled.
+ 
+    The following code will crash because of infinite recursion:
+ 
+    @code
+    [[inv target] performSelector:[inv selector]]
+    @endcode
+ 
+    You can't call the selector that advice is running for without causing infinite recursion.
+    If you want to call actual method the advice is running for, use <tt>[inv invoke]</tt>
+    without modifying the target, selector or IMP.
 */
 @protocol AOCAdviceProtocol <NSObject>
 @optional
 
 /*!
-    @abstract   Where "before" advice is executed
-    @discussion The invocation argument will be invoked after this method is called,
-                with any modification made to it.
-    @param      inv The invocation before which this method is executing
+    Where "before" advice is executed.
+ 
+    The invocation argument will be invoked after this method is called,
+    with any modification made to it. This means that the arguments may
+    be modified here before the actual method is invoked.
+ 
+    @param  inv The invocation before which this method is executing.
 */
 - (void) adviceBefore:(id<AOCInvocationProtocol>)inv;
+
 /*!
-    @abstract   Where the "instead of" advice is executed
-    @discussion The invocation argument is only invoked if this method returns YES.
-                Otherwise, the return value of the invocation should be set manually
-                by calling either -invoke or -setReturnValue:
-    @param      inv The invocation instead of which this method is executing
-    @result     YES if the invocation argument should still be invoked after this method returns, otherwise NO
+    Where the "instead of" advice is executed.
+ 
+    The invocation argument is only invoked if this method returns YES.
+    Otherwise, the return value of the invocation should be set manually
+    by calling either AOCInvocationProtocol#invoke or AOCInvocationProtocol#setReturnValue:
+ 
+    @param  inv The invocation instead of which this method is executing
+    @result YES if the invocation argument should still be invoked after
+            this method returns, otherwise NO
 */
 - (BOOL) adviceInsteadOf:(id<AOCInvocationProtocol>)inv;
 
 /*!
-    @abstract   Where "after" advice is executed
-    @discussion The invocation argument will be invoked after this method is called,
-                with any modification made to it.
-    @param      inv The invocation after which this method is executing
+    Where "after" advice is executed.
+ 
+    The invocation argument should have already been invoked by the time
+    this method is called. The return value of the invocation argument
+    may be modified here.
+ 
+    @param  inv The invocation after which this method is executing
 */
 - (void) adviceAfter:(id<AOCInvocationProtocol>)inv;
 @end
 
 
-/*!
-    @abstract    An implementation of the AOCAdviceProtocol for convenience
-    @discussion  The AOCAdvice class invokes "before", "instead of", and "after"
-                 methods with the same arguments as the actual method that the
-                 advice is running for. For example, if the advice is installed
-                 for the selector:
+/*! 
+    A convenient base class for advice objects.
  
-                 -(double)divide:(double)numerator by:(double)denominator;
+    Advice objects needn't be a subclass of AOCAdvice, they must simply
+    implement AOCAdviceProtocol. AOCAdvice is only provided for convenience.
  
-                 AOCAdvice will run these methods on itself if they exist:
- 
-                 -(double)adviceBeforeDivide:(double)numerator by:(double)denominator;
-                 -(double)adviceInsteadOfDivide:(double)numerator by:(double)denominator;
-                 -(double)adviceAfterDivide:(double)numerator by:(double)denominator;
- 
-                 The return values of -adviceBefore<sel> and -adviceAfter<sel> are ignored.
- 
-                 In -adviceBefore<sel>, the arguments may be modified via 
-                 [[self invocation] setArgument:... atIndex:...] before the actual method is
-                 called. Be aware that the first argument is at index 2.
- 
-                 If -adviceInsteadOf<sel> is implemented, then the advice will replace
-                 the actual method. The actual method will not be called, and the
-                 return value from -adviceInsteadOf<sel> is treated as the actual return value.
- 
-                 In -adviceAfter<sel>, the return value may be modified via
-                 [[self invocation] setReturnValue:...] after the actual method has been
-                 called. You can also retreive the return value of the actual method via
-                 [[self invocation] getReturnValue:...]
+    The AOCAdvice class invokes "before", "instead of", and "after"
+    methods with the same arguments as the actual method that the
+    advice is running for. For example, if the advice is installed
+    for the selector:
+
+    @code
+    -(double)divide:(double)numerator by:(double)denominator;
+    @endcode
+
+    AOCAdvice will run these methods on itself if they exist:
+
+    @code
+    -(double)adviceBeforeDivide:(double)numerator by:(double)denominator;
+    -(double)adviceInsteadOfDivide:(double)numerator by:(double)denominator;
+    -(double)adviceAfterDivide:(double)numerator by:(double)denominator;
+    @endcode
+
+    The return values of <tt>-adviceBefore<sel></tt> and <tt>-adviceAfter<sel></tt> are ignored.
+
+    In <tt>-adviceBefore<sel></tt>, the arguments may be modified via 
+    <tt>[[self invocation] setArgument:... atIndex:...]</tt> before the actual method is
+    called. Be aware that the first argument is at index 2.
+
+    If <tt>-adviceInsteadOf<sel></tt> is implemented, then the advice will replace
+    the actual method. The actual method will not be called, and the
+    return value from -adviceInsteadOf<sel> is treated as the actual return value.
+
+    In <tt>-adviceAfter<sel></tt>, the return value may be modified via
+    <tt>[[self invocation] setReturnValue:...]</tt> after the actual method has been
+    called. You can also retreive the return value of the actual method via
+    <tt>[[self invocation] getReturnValue:...]</tt>.
 */
 @interface AOCAdvice : NSObject<AOCAdviceProtocol> {
     id<AOCInvocationProtocol> _inv;
 }
 
 /*!
-    @result     The invocation representing the actual method that the advice is running for, 
-                or nil if not called from within an advice method.
+    @result The invocation representing the actual method that the advice is running for, 
+            or nil if not called from within an advice method.
+    @see AOCInvocationProtocol
 */
 - (id<AOCInvocationProtocol>) invocation;
 
--- AspectObjectiveC/src/AOCInvocationProtocol.h
+++ AspectObjectiveC/src/AOCInvocationProtocol.h
@@ -1,18 +1,116 @@
 
 #import <Cocoa/Cocoa.h>
 
+/*!
+    Class similar to NSInvocation.
+ 
+    Classes conforming to AOCInvocationProtocol work
+    almost exactly like NSInvocation objects.
 
+    The main difference between NSInvocation and
+    AOCInvocationProtocol objects is that 
+    AOCInvocationProtocol objects can take a specific
+    IMP to execute. This is useful when you want to
+    run a specific method regardless of whether it is
+    attached to a selector or not. This is important in
+    AspectObjectiveC because methods are swizzled, and the
+    correct IMP may not be the one attached to the selector.
+
+    AOCInvocationProtocol implement NSCopying.
+    NSInvocation also implements NSCopying, but it
+    doesn't copy the arguments. AOCInvocationProtocol
+    copies everything properly.
+*/
 @protocol AOCInvocationProtocol <NSObject, NSCopying>
--(void) setSelector:(SEL)selector;
--(SEL) selector;
--(void) setTarget:(id)target;
--(id) target;
--(IMP) imp;
--(void) setImp:(IMP)imp;
--(void) setArgument:(void*)argPtr atIndex:(NSUInteger)idx;
--(void) getArgument:(void*)argPtr atIndex:(NSUInteger)idx;
--(void) setReturnValue:(void*)returnValPtr;
--(void) getReturnValue:(void*)returnValPtr;
--(size_t) returnValueSize;
--(void) invoke;
+
+
+/*!
+    Sets the selector of the invocation (_cmd variable).
+ 
+    Unlike NSInvocation, this may not change the method that
+    is invoked. If <tt>[self imp] == nil</tt>, then <tt>[self invoke]</tt> will
+    work exactly the same as NSInvocation. If <tt>[self imp]</tt> is not
+    nil, then <tt>[self imp]</tt> will be invoked regardless of what
+    [self selector] is.
+    @see    setImp:
+    @param  selector    The selector to invoke
+*/
+- (void) setSelector:(SEL)selector;
+
+/*!
+    The selector to invoke.
+    @see setSelector:
+*/
+- (SEL) selector;
+
+/*!
+    Sets the object of the invocation (self variable).
+    @param  target  The target to invoke with
+*/
+- (void) setTarget:(id)target;
+
+/*!
+    The target to invoke with.
+*/
+- (id) target;
+
+/*!
+    Sets the IMP to invoke.
+ 
+    When <tt>[self imp]</tt> is set to NULL, the <tt>[self invoke]</tt> will
+    behave like NSInvocation and invoke the IMP returned from:
+ 
+    @code
+    [[self target] methodForSelector:[self selector]]
+    @endcode
+
+    However, <tt>[self invoke]</tt> will execute <tt>[self imp]</tt> if it is not NULL.
+    The IMP will be passed <tt>[self target]</tt> and <tt>[self selector]</tt> as
+    <tt>self</tt> and <tt>_cmd</tt> respectively.
+ 
+    @param  imp The IMP to invoke, or NULL if the IMP should be looked up
+                with the target and selector like NSInvocation does
+*/
+- (void) setImp:(IMP)imp;
+
+/*!
+    The IMP to invoke (can be NULL).
+    @see setImp:
+*/
+- (IMP) imp;
+
+/*!
+    Works exactly like <tt>-[NSInvocation setArgument:atIndex:]</tt>.
+*/
+- (void) setArgument:(void*)argPtr atIndex:(NSUInteger)idx;
+
+/*!
+    Works exactly like <tt>-[NSInvocation getArgument:atIndex:]</tt>.
+ */
+- (void) getArgument:(void*)argPtr atIndex:(NSUInteger)idx;
+
+/*!
+    Works exactly like <tt>-[NSInvocation setReturnValue:]</tt>.
+ */
+- (void) setReturnValue:(void*)returnValPtr;
+
+/*!
+    Works exactly like <tt>-[NSInvocation getReturnValue:]</tt>.
+ */
+- (void) getReturnValue:(void*)returnValPtr;
+
+/*!
+    The number of bytes required to hold the return value.
+ */
+- (size_t) returnValueSize;
+
+/*!
+    Works (almost) like <tt>-[NSInvocation invoke]</tt>.
+ 
+    If <tt>[self imp]</tt> is NULL, then this method works exactly like 
+    NSInvocation. Otherwise, <tt>[self imp]</tt> is invoked instead of
+    the actual IMP attached to the selector of the target.
+ */
+- (void) invoke;
+
 @end
